52f6cd6442dec6e21d001ca779c2593fbff3eacb
ÉmilioRosso <7zen@ukr.net>
2020-03-05 09:11:49 +0800

    Updates

---
+++ precache-manifest.1852a1e7e453510d58c000bc84183a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742f3c1508c61413c4b795ed62d5e92a",
+    "url": "/goit-react-hw-05-phonebook-ctx/index.html"
+  },
+  {
+    "revision": "b1be832fdcbf78387acb",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/css/main.4657e348.chunk.css"
+  },
+  {
+    "revision": "b0b1266a6416c0276b91",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.21305e2e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.21305e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1be832fdcbf78387acb",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/main.e6b4bbf9.chunk.js"
+  },
+  {
+    "revision": "471224084cf842a1de0a",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/runtime-main.0181941c.js"
+  }
+]);
